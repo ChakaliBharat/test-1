3463d5228689dbb13262282245676d7ea1a6789d
unknown <chakalibharath99@gmail.com>
2024-08-04 00:57:33 +0530

    rename the bass

--- first-test-project/src/App.jsx
+++ first-test-project/src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>HI i am barath the bass</>;
+  return <>HI i am barath the boss</>;
 }
 
 export default App;
